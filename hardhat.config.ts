77dd97b218c08105b16535aa3f0086501d34934c
Dhanushkms <143531988+Dhanushkms@users.noreply.github.com>
2024-04-15 00:05:47 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,8 @@ import "@nomicfoundation/hardhat-toolbox";
 // https://github.com/projectsophon/hardhat-circom
 import "hardhat-circom";
 // circuits
-import circuits = require('./circuits.config.json')
+import circuits = require('./circuits.config.json');
+// import 'dotenv/config';
 
 // set env var to the root of the project
 process.env.BASE_PATH = __dirname;
@@ -22,8 +23,14 @@ const config: HardhatUserConfig = {
       }
     ]
   },
+  networks: {
+    sepolia: {
+      url: 'https://rpc.sepolia.org',
+       accounts: [process.env.Private_key!],
+    },
+  },
   circom: {
-    // (optional) Base path for input files, defaults to `./circuits/`
+    // (optional) Base path for input files, defaults to ./circuits/
     inputBasePath: "./circuits",
     // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
     ptau: "powersOfTau28_hez_final_12.ptau",
